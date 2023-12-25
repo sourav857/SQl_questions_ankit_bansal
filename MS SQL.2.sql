eae86486368469113356c8ec91a25d5ea58de089
sourav857 <123013290+sourav857@users.noreply.github.com>
2023-12-25 13:10:48 +0530

    Add files via upload

---
+++ MS SQL.2.sql
@@ -0,0 +1,51 @@
+create table products
+(
+product_id varchar(20) ,
+cost int
+);
+insert into products values ('P1',200),('P2',300),('P3',500),('P4',800);
+
+create table customer_budget
+(
+customer_id int,
+budget int
+);
+
+insert into customer_budget values (100,400),(200,800),(300,1500);
+
+
+
+
+
+
+with cte as 
+(
+select *,sum(cost) over(order by cost) as cum_sum 
+from products 
+  )
+select customer_id,count(*) as counting,string_agg(product_id,',') as number_of_products from customer_budget cb 
+left join cte c ON
+c.cum_sum<cb.budget
+group by customer_id
+
+
+
+
+
+
+
+  
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
