5874bb56d9efc6e2781dd691a61e61af34c176ad
sourav857 <123013290+sourav857@users.noreply.github.com>
2023-12-25 13:57:02 +0530

    Add files via upload

---
+++ MS SQL.3.sql
@@ -0,0 +1,28 @@
+/*CREATE TABLE subscriber (
+ sms_date date ,
+ sender varchar(20) ,
+ receiver varchar(20) ,
+ sms_no int
+);
+-- insert some values
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Vibhor',10);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Avinash', 'Pawan',30);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Avinash',20);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Pawan',5);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Pawan', 'Vibhor',8);
+INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Deepak',50);
+
+-Find total number of messages exchanges between each person per day
+*/
+with cte as (
+select *,
+case when sender<receiver then sender else receiver end as s1,
+ case   when sender>receiver then sender else receiver end as s2
+	  from subscriber )
+      select  sms_date,s1,s2,sum(sms_no) as messages_sent from cte 
+      group BY sms_date,s1,s2
+
+
+
+
