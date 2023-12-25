52123ef49a30955a9c98cc91a363f6f27897b905
sourav857 <123013290+sourav857@users.noreply.github.com>
2023-12-25 13:59:19 +0530

    Update MS SQL.3.sql

--- MS SQL.3.sql
+++ MS SQL.3.sql
@@ -15,13 +15,20 @@ INSERT INTO subscriber VALUES ('2020-4-1', 'Vibhor', 'Deepak',50);
 
 -Find total number of messages exchanges between each person per day
 */
-with cte as (
-select *,
-case when sender<receiver then sender else receiver end as s1,
- case   when sender>receiver then sender else receiver end as s2
-	  from subscriber )
-      select  sms_date,s1,s2,sum(sms_no) as messages_sent from cte 
-      group BY sms_date,s1,s2
+with 
+cte as (
+	select *,
+	case when sender<receiver then sender else receiver end as s1,
+ 	case when sender>receiver then sender else receiver end as s2
+	from subscriber 
+	)
+select sms_date,s1,s2,sum(sms_no) 
+as 
+messages_sent 
+from 
+cte 
+group BY 
+sms_date,s1,s2
 
 
 
