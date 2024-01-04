e7124959b7397781d4f5c3b30a72ec03cb39c843
sourav857 <123013290+sourav857@users.noreply.github.com>
2024-01-04 08:24:06 +0530

    practice_questions.sql

--- MS SQL.sql
+++ MS SQL.sql
@@ -31,40 +31,36 @@ insert into clocked_hours values
 with cte as 
 (	
   	select 	
-  		*,row_number() over(partition by empd_id,flag order by swipe )
+  	*,row_number() over(partition by empd_id,flag order by swipe )
   	as 
-  		match 
-	from 
-		clocked_hours 
+  	match 
+	from clocked_hours 
 ),
 cte1 as 
 	(
-		select 
-  			c1.empd_id,datediff(hour,c1.swipe,c2.swipe) 
-		as 
- 			time 
-		from 
-  			cte c1 
-		inner join 
-  			cte c2 
-		on 1=1 
-		where 
-		c1.swipe!=c2.swipe 
-  		and 
-			c1.empd_id=c2.empd_id 
-  		and 
-			c1.match=c2.match
+	select 
+  	c1.empd_id,datediff(hour,c1.swipe,c2.swipe) 
+	as 
+ 	time 
+	from 
+  	cte c1 
+	inner join 
+  	cte c2 
+	on 1=1 
+	where 
+	c1.swipe!=c2.swipe 
+  	and 
+	c1.empd_id=c2.empd_id 
+  	and 
+	c1.match=c2.match
   	)
   select 
-	empd_id,sum(time) 
+  empd_id,sum(time) 
   as 
-	total_time 
-  from 
-	cte1 
-  where 
-	time>0 
-  group by 
-	empd_id
+  total_time 
+  from cte1 
+  where time>0 
+  group by empd_id
 
 
 --- rank method
@@ -78,38 +74,37 @@ cte1 as
   ,
   cte1 AS
    (
-  	select 
-     		empd_id,max(swipe) as swipe_in,min(swipe) as swipe_out,
-  			datediff(hour,min(swipe),max(swipe) ) as time
+  	select empd_id,max(swipe) as swipe_in,min(swipe) as swipe_out,
+  	datediff(hour,min(swipe),max(swipe) ) as time
   	from 
      	cte 
-    group by 
+	group by 
      	rn,empd_id
     )
     
     select 
-    	empd_id,sum(time) 
+    empd_id,sum(time) 
     as 
-    	total_time 
+    total_time 
     from 
-    	cte1 
+    cte1 
     group by 
-    	empd_id
+    empd_id
     
 ---lead method
   
     WITH CTE AS 
-    	( 
-          select 
-          *, lead(swipe,1) over(partition by empd_id order by swipe) 
-          as next_swipe
-    	  from clocked_hours
+    ( 
+      select 
+       *, lead(swipe,1) over(partition by empd_id order by swipe) 
+       as next_swipe
+       from clocked_hours
         )
  	select 
  	empd_id,sum(datediff(hour,swipe,next_swipe)) 
-    as toal_punch_time 
-    from cte
-    where flag= 'I'
+        as toal_punch_time 
+        from cte
+        where flag= 'I'
  	group by empd_id
     
     
